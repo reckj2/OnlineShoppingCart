1f8e06c4d901632f65244a25ab7e741e36f8a113
reckj2 <42847675+reckj2@users.noreply.github.com>
2020-01-03 16:13:54 -0700

    Update ShoppingCart.cpp

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -1,16 +1,29 @@
+/*
+* File: ShoppingCart.cpp
+* Author: Joshua Reck
+* NetID: reckj2
+*
+* Description: Creates an ‘online’ shopping cart that prompts users for entries.
+*
+*/
+
 #include "ShoppingCart.h"
 #include "ItemToPurchase.h"
 
+//Default constructor
 ShoppingCart::ShoppingCart() {
 	customerName = "none";
 	currentDate = "January 1, 2016";
 }
 
+//Parametized constructor
 ShoppingCart::ShoppingCart(string name, string date) {
 	customerName = name;
 	currentDate = date;
 }
 
+
+//Getters and setters 
 string ShoppingCart::GetCustomerName() const {
 	return customerName;
 }
@@ -27,12 +40,15 @@ void ShoppingCart::setDate(string date) {
 	currentDate = date;
 }
 
+//Adds item of class ItemToPurchase to cartItems
 void ShoppingCart::AddItem(ItemToPurchase item) {
 	cartItems.push_back(item);
 }
 
+//Removes item of class ItemToPurchase from cartItems
 void ShoppingCart::RemoveItem(string name) {
 	int p=0;
+	//Cycles through list of items and compares name of item
 	for (unsigned int i = 0; i < cartItems.size(); i++) {
 		if (name.compare(cartItems[i].GetName()) == 0) {
 			cartItems.erase(cartItems.begin() + i);
@@ -44,12 +60,14 @@ void ShoppingCart::RemoveItem(string name) {
 	}
 }
 
+//Modify items in shopping cart given type ItemToPurchase
 void ShoppingCart::ModifyItem(ItemToPurchase item) {
 	int p = 0;
 	int check = 0;
 	cout << "Enter the new quantity:" << endl;
 	cin >> p;
-
+	
+	//Cycles through items and changes quantity
 	for (unsigned int i = 0; i < cartItems.size(); i++) {
 		if (item.GetName().compare(cartItems[i].GetName()) == 0) {
 			check = 1;
@@ -61,6 +79,7 @@ void ShoppingCart::ModifyItem(ItemToPurchase item) {
 	}
 }
 
+//returns number of items in cart
 int ShoppingCart::GetNumItemsInCart() const{
 	int temp = 0;
 	for (unsigned int i = 0; i < cartItems.size(); i++) {
@@ -69,6 +88,7 @@ int ShoppingCart::GetNumItemsInCart() const{
 	return temp;
 }
 
+//Adds up total cost of cart
 int ShoppingCart::GetCostOfCart() const{
 	int temp =0;
 	for (unsigned int i = 0; i < cartItems.size(); i++) {
@@ -77,10 +97,11 @@ int ShoppingCart::GetCostOfCart() const{
 	return temp;
 }
 
+
+//Prints out each item, the quantity and the total price
 void ShoppingCart::PrintTotal() {
 	int temp;
-
-
+	
 	if (cartItems.size() == 0) {
 		cout << "SHOPPING CART IS EMPTY" << endl;
 	}
@@ -89,14 +110,13 @@ void ShoppingCart::PrintTotal() {
 		for (unsigned int i = 0; i < cartItems.size(); i++) {
 			temp = cartItems[i].GetQuantity() *cartItems[i].GetPrice();
 			cout << cartItems[i].GetName() << " " << cartItems[i].GetQuantity() << " @ $" << cartItems[i].GetPrice() << " = $" << temp << endl;
-			//temp2 = temp2 + temp;
+			
 		}
-
-	//	cout << "Total: $" << temp2 << endl;
 	}
 
 }
 
+//Prints items and their decriptions
 void ShoppingCart::PrintDescriptions() {
 
 	if (cartItems.size() == 0) {
